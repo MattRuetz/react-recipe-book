83757adfe2a0dc1b1a5b485c0add6a9dfdb5f412
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-17 16:25:12 -0400

    routes added to App component for each page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Create from './pages/create/Create';
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/create" element={<Create />} />
+                    <Route path="/recipes/:id" element={<Recipe />} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="*" element={<h1>404: NOT FOUND</h1>} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
-export default App
+export default App;
