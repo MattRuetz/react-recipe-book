2343cf388747d5cd0cb1c024e25aa0d0991395df
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-18 14:01:17 -0400

    fetch from json DB to fill home page with recipes and individual recipe pages

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Create from './pages/create/Create';
 import Home from './pages/home/Home';
 import Recipe from './pages/recipe/Recipe';
 import Search from './pages/search/Search';
+import Navbar from './components/Navbar';
 
 function App() {
     return (
         <div className="App">
             <Router>
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/create" element={<Create />} />
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,24 @@
+import { useParams } from 'react-router-dom';
+
 import './Recipe.css';
+import useFetch from '../../hooks/useFetch';
 
 function Recipe() {
-    return <div>Recipe</div>;
+    const { id } = useParams();
+
+    const {
+        data: recipe,
+        isPending,
+        error,
+    } = useFetch(`http://localhost:3000/recipes/${id}`);
+
+    return (
+        <div className="recipe">
+            {error && <p className="error">{error}</p>}
+            {isPending && <p className="loading">Loading...</p>}
+            {recipe && <h3>{recipe.title}</h3>}
+        </div>
+    );
 }
 
 export default Recipe;
