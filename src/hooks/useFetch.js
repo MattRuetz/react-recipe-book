30d7b1ce32db437ed5e91de0ede191a27da969dd
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-18 16:13:07 -0400

    added POST functionality to useFetch hook with useFetch.postData exported function

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,23 +1,42 @@
 import { useState, useEffect } from 'react';
 
-const useFetch = (url) => {
+// Takes endpoint URL and method (optional, GET by default)
+const useFetch = (url, method = 'GET') => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(false);
     const [error, setError] = useState(null);
+    const [options, setOptions] = useState(null);
+
+    const postData = (postData) => {
+        setIsPending(true);
+
+        setOptions({
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData),
+        });
+    };
 
     useEffect(() => {
         const controller = new AbortController();
 
-        const fetchData = async () => {
+        const fetchData = async (fetchOptions) => {
             setIsPending(true);
 
             try {
-                // Good fetch
-                const res = await fetch(url, { signal: controller.signal });
+                // Successful GET OR POST (based on fetchOptions.method)
+                const res = await fetch(url, {
+                    ...fetchOptions,
+                    signal: controller.signal,
+                });
+
                 if (res.status !== 200 /* not ok*/) {
                     // escape to catch block
                     throw new Error(res.statusText);
                 }
+
                 const json = await res.json();
                 setData(json);
                 setError(null);
@@ -32,15 +51,20 @@ const useFetch = (url) => {
             setIsPending(false);
         };
 
-        fetchData();
+        // Check if method is GET or POST request
+        if (method === 'GET') {
+            fetchData();
+        } else if (method === 'POST' && options) {
+            fetchData(options); // postData() was called from somewhere
+        }
 
         // Return a cleanup function so can catch update to unmounted component
         return () => {
             controller.abort();
         };
-    }, [url]);
+    }, [url, options, method]);
 
-    return { data, isPending, error };
+    return { data, isPending, error, postData };
 };
 
 export default useFetch;
