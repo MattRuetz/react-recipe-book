4a3b422b746cd11779ff6eee93607e975a424de7
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-18 21:25:34 -0400

    simple context demo -- see context/ThemeContext.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
-import SearchBar from './SearchBar';
+import { useContext } from 'react';
+// Styles
 import './Navbar.css';
+// Components
+import { ThemeContext } from '../context/ThemeContext';
+import SearchBar from './SearchBar';
 
 function Navbar() {
+    const { color } = useContext(ThemeContext);
+
     return (
-        <div className="navbar">
+        <div className="navbar" style={{ background: color }}>
             <nav>
                 <Link to="/" className="brand">
                     <h1>Cooking Ninja</h1>
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,12 @@
+// Contxt for light/dark theme
+import { createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+    return (
+        <ThemeContext.Provider value={{ color: 'blue' }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
